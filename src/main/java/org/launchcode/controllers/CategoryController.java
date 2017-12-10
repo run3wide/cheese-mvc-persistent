cfb8afdbd4e25336e7d70bca3d608f0d35f143fb
paul <paulvrobson@gmail.com>
2017-12-10 04:04:54 -0600

    Add feature "Add Category" -add view -add GET and POST handler methods

--- src/main/java/org/launchcode/controllers/CategoryController.java
+++ src/main/java/org/launchcode/controllers/CategoryController.java
@@ -1,10 +1,16 @@
 package org.launchcode.controllers;
 
+import org.launchcode.models.Category;
 import org.launchcode.models.data.CategoryDao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.validation.Valid;
 
 @Controller
 @RequestMapping(value = "category")
@@ -18,6 +24,25 @@ public class CategoryController {
         model.addAttribute("categories", categoryDao.findAll());
         model.addAttribute("title", "All Categories");
         return "category/index";
+    }
+
+    @RequestMapping(value = "/add")
+    public String displayAddForm(Model model) {
+        model.addAttribute("title", "Add Category");
+        model.addAttribute(new Category());
+
+        return "category/add";
+    }
+
+    @RequestMapping(value = "/add", method = RequestMethod.POST)
+    public String addCategory(Model model, @ModelAttribute @Valid Category category, Errors errors) {
+        if (errors.hasErrors()) {
+            model.addAttribute("title", "Add Category");
+            return "category/add";
+        }
+
+        categoryDao.save(category);
+        return "redirect:";
     }
 
 }
