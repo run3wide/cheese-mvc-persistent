12875a04cb13228d32f614a04681392a43d87ae5
paul <paulvrobson@gmail.com>
2017-12-10 03:51:02 -0600

    Add index view for category -add method handler

---
+++ src/main/java/org/launchcode/controllers/CategoryController.java
@@ -0,0 +1,23 @@
+package org.launchcode.controllers;
+
+import org.launchcode.models.data.CategoryDao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping(value = "category")
+public class CategoryController {
+
+    @Autowired
+    private CategoryDao categoryDao;
+
+    @RequestMapping(value = "")
+    public String index(Model model) {
+        model.addAttribute("categories", categoryDao.findAll());
+        model.addAttribute("title", "All Categories");
+        return "category/index";
+    }
+
+}
